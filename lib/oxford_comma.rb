1f3444a75cb960dd83b3b1612e7dbd6d4e78472b
Eli Huebner <elimhuebner@gmail.com>
2020-09-09 18:47:19 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,10 @@
 def oxford_comma(array)
-
+  if array.length == 1
+    array.join
+  elsif array.length == 2
+    array.join (" and ")
+  else
+    last_fruit = array.pop
+    array.join(", ") << ", and #{last_fruit}"
+  end
 end
