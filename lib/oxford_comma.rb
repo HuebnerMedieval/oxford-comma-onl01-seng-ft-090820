919b80e03fe5abc0025a7dd87cfa47d9429f5891
Eli Huebner <elimhuebner@gmail.com>
2020-09-14 18:31:56 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -4,7 +4,16 @@ def oxford_comma(array)
   elsif array.length == 2
     array.join (" and ")
   else
-    last_fruit = array.pop
-    array.join(", ") << ", and #{last_fruit}"
+    # last_fruit = array.pop
+    # array.join(", ") << ", and #{last_fruit}"
+    
+    new_array = array.map do |word, index|
+    if index < (array.length - 1)
+        word + ", "
+    elsif index == (array.length - 1)
+        "and" + array[index]
+    end
+    new_array.join
   end
+end
 end
